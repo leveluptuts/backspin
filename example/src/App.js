9ca339fe61363468cb2a654fecf38e5288eae7c7
Scott Tolinski <scott.tolinski@gmail.com>
2019-08-01 22:39:07 -0600

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'backspin'
+import ExampleComponent from 'backspin';
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <h1>Backspin</h1>
+        <h3>Admin Toolbar</h3>
+        <ExampleComponent text="Modern React component module" />
       </div>
-    )
+    );
   }
 }
